242d779352a40a84b9eca0055279b73e3fd3dc10
Justyna Oleś <justynaoles88@gmail.com>
2019-01-24 20:25:15 +0100

    Task 2

--- src/js/index.js
+++ src/js/index.js
@@ -7,6 +7,12 @@ document.addEventListener('DOMContentLoaded', (event)=>{
     const btnClose = document.querySelector('.btn-close');
     const pop = document.querySelector('.popup');
     const submit = document.querySelector('.submit');
+    const form = document.querySelector('form');
+    const formWrapper = form.querySelector('.form-wrapper');
+    const inputs = form.querySelectorAll('input');
+    const checkbox = form.querySelector('.checkbox-tick');
+    const label = form.querySelector('.label');
+
     
     
     
@@ -37,6 +43,131 @@ document.addEventListener('DOMContentLoaded', (event)=>{
         },200);
     
     });
+
+    checkbox.addEventListener('click', function(){
+
+        if(label.classList.contains('valid')) {
+    
+            label.classList.remove('valid');
+            label.classList.add('invalid');
+        }
+    
+        else {
+    
+            label.classList.add('valid');
+            label.classList.remove('invalid');
+    
+        }
+    
+    });
+    
+    
+    function correctClass(item){
+    
+        item.classList.add('valid');
+        item.classList.remove('invalid');
+    
+    }
+    
+    function errorClass(item){
+    
+        item.classList.remove('valid');
+        item.classList.add('invalid');
+    }
+    
+    
+    formWrapper.addEventListener('click', function(e) {
+    
+        let input = e.target;
+    
+        input.addEventListener('blur', () =>{
+    
+            let text = input.value;
+            let textString = text.toString();
+            let textLength = textString.length;
+            let type = input.type;
+            const emailPattern = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+            let isEmail = emailPattern.test(textString);
+            const textPattern = /[a-zA-Z]/;
+            const isText = textPattern.test(textString);
+    
+    
+        if (type =='password')  {
+    
+                if(textLength > 6 && isText) {
+    
+                    correctClass(input);
+                }
+    
+                else {
+                
+                    errorClass(input);
+                    alert("Please use at least 6 charts");
+    
+                } 
+            }
+    
+        if (type =='email')  {
+        
+                if(isEmail) {
+    
+                    correctClass(input);
+                }
+    
+                else {
+                
+                    errorClass(input);
+                } 
+            }
+    
+        });
+    
+    
+    });
+    
+    
+    form.addEventListener('submit', function(e){
+    
+    e.preventDefault();
+    
+    let errors = 0;
+    
+        for (let i = 0; i < inputs.length; i++) {
+    
+            if (inputs[i].type=="password" && inputs[i].value=="" ) {
+    
+                errors++;
+                errorClass(inputs[i]);
+                    
+                }
+    
+                if (inputs[i].type=="email" && inputs[i].value==""){
+    
+                    errors++;
+                    errorClass(inputs[i]);
+                }
+    
+    
+                if (inputs[i].type=="checkbox" && (inputs[i].checked == false)) {
+    
+                    errors++;
+                    errorClass(label);  
+                }
+        }
+    
+    
+        if (errors>0) {
+    
+            alert("Please fill in all fields");
+        }
+    
+        else {
+    
+           form.submit(); 
+        }
+    
+    });
+    
 
 
 })

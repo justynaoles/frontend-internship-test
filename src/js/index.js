7fadb144850888d8a95dacb27d5af29656f1be13
Justyna Oleś <justynaoles88@gmail.com>
2019-01-24 20:15:31 +0100

    Task1

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,50 @@
 /* Here goes your JS code */
+
+
+document.addEventListener('DOMContentLoaded', (event)=>{
+
+    const btnMain = document.querySelector('.btn-main');
+    const btnClose = document.querySelector('.btn-close');
+    const pop = document.querySelector('.popup');
+    const submit = document.querySelector('.submit');
+    
+    
+    
+    function hideButton() {
+    
+        btnMain.classList.add('hide');
+        pop.classList.add('show');
+    
+        setTimeout(function(){
+    
+            pop.classList.add('opacity');
+    
+        }, 300);
+    }
+    
+    btnMain.addEventListener('click', hideButton);
+    
+    
+    btnClose.addEventListener('click', () =>{
+    
+        btnMain.classList.remove('hide');
+        pop.classList.remove('opacity');
+    
+        setTimeout(function(){
+    
+            pop.classList.remove('show');
+        
+        },200);
+    
+    });
+
+
+})
+  
+
+
+
+
+
+
+
